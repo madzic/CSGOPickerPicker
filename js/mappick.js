d7bd9b9db8ec95cb0e629560bb0b5b1a3f52231c
Milan Adzic <pajsernakvadrat@gmail.com>
2019-10-07 16:54:56 +0200

    Update mappick.js

--- js/mappick.js
+++ js/mappick.js
@@ -9,10 +9,6 @@ function shuffle(a) {
 function randomMap(){
 
   var values = [];
-  var values1 = [];
-  var selectedcore = [];
-  var selectedother = [];
-  var n = document.getElementById('numpick').value;
 
   var cbs = document.forms['coremaps'].elements['maps'];
   for(var i=0,cbLen=cbs.length;i<cbLen;i++){
@@ -22,22 +18,7 @@ function randomMap(){
   }
 
   shuffle(values);
-  selectedcore = values.slice(0,n); 
-  
-  var cbt = document.forms['othermaps'].elements['others'];
-  for(var i=0,cbLen=cbt.length;i<cbLen;i++){
-    if(cbt[i].checked){
-      values1.push(cbt[i].value);
-    } 
-  }
-  
-  shuffle(values1);
-  selectedother = values1.slice(0,4-n);
+  selectedcore = values.slice(0,1); 
 
-  var selectedtotal = selectedcore.concat(selectedother);
-  shuffle(selectedtotal);
-
-  document.getElementById('result').innerHTML = "Core maps: " + selectedcore.toString()//values.slice(0,n).toString();
-  document.getElementById('result2').innerHTML = "Other pool maps: " +selectedother.toString()//values.slice(0,n).toString();
-  document.getElementById('result3').innerHTML = "Play row: " + selectedtotal.toString()//values.slice(0,n).toString();  
+  document.getElementById('result3').innerHTML = "Maps will choose: " + selectedcore.toString()//values.slice(0,n).toString();  
 };
